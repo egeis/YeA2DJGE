3aef42de48f6bb71b75ee1491307681b7b964d9a
egeis <rcoan@callutheran.edu>
2015-03-01 20:14:13 -0800

    Failed implementation of OpenGL.

--- src/game/Game.java
+++ src/game/Game.java
@@ -25,21 +25,35 @@ package game;
 
 import game.io.ConfigHandler;
 import game.io.InputHandler;
-import java.awt.Canvas;
-import java.awt.Dimension;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+ 
+import java.nio.ByteBuffer;
+ 
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  *
  * @author Richard Coan
  */
-public class Game extends Canvas implements Runnable {
+public class Game implements Runnable {
     private static Game instance = null;
     private static final Logger LOG = LogManager.getLogger( Game.class.getName() );
         
     protected ConfigHandler Config;
     protected InputHandler Input;
+    
+//    private GLFWErrorCallback errorCallback;
+//    private GLFWKeyCallback   keyCallback;
+    
+    private long window;
 
     public static Game getInstance() {
         if(instance == null) instance = new Game();
@@ -53,7 +67,101 @@ public class Game extends Canvas implements Runnable {
     
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+ 
+        try {
+            init();
+            loop(); 
+            
+            // Release window and window callbacks
+//            glfwDestroyWindow(window);
+//            keyCallback.release();
+        } finally {
+            // Terminate GLFW and release the GLFWerrorfun
+//            glfwTerminate();
+//            errorCallback.release();
+        }
+    }
+ 
+    private void init() {
+//        // Setup an error callback. The default implementation
+//        // will print the error message in System.err.
+//        glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+// 
+//        // Initialize GLFW. Most GLFW functions will not work before doing this.
+//        if ( glfwInit() != GL11.GL_TRUE )
+//            throw new IllegalStateException("Unable to initialize GLFW");
+// 
+//        // Configure our window
+//        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+//        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+//        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+ 
+        int WIDTH = 300;
+        int HEIGHT = 300;
+ 
+//        // Create the window
+//        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
+//        if ( window == NULL )
+//            throw new RuntimeException("Failed to create the GLFW window");
+// 
+//        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+//        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+//            @Override
+//            public void invoke(long window, int key, int scancode, int action, int mods) {
+//                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+//                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+//            }
+//        });
+// 
+//        // Get the resolution of the primary monitor
+//        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+//        // Center our window
+//        glfwSetWindowPos(
+//            window,
+//            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+//            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+//        );
+// 
+//        // Make the OpenGL context current
+//        glfwMakeContextCurrent(window);
+//        // Enable v-sync
+//        glfwSwapInterval(1);
+// 
+//        // Make the window visible
+//        glfwShowWindow(window);
+    }
+ 
+    private void loop() {
+//        // This line is critical for LWJGL's interoperation with GLFW's
+//        // OpenGL context, or any context that is managed externally.
+//        // LWJGL detects the context that is current in the current thread,
+//        // creates the ContextCapabilities instance and makes the OpenGL
+//        // bindings available for use.
+//        GLContext.createFromCurrent();
+//        
+//        // Run the rendering loop until the user has attempted to close
+//        // the window or has pressed the ESCAPE key.
+//        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
+//            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+//
+//            // set the color of the quad (R,G,B,A)
+//            GL11.glColor3f(0.5f,0.5f,1.0f);
+//
+//            // draw quad
+//            GL11.glBegin(GL11.GL_QUADS);
+//                GL11.glVertex2f(100,100);
+//                GL11.glVertex2f(100+200,100);
+//                GL11.glVertex2f(100+200,100+200);
+//                GL11.glVertex2f(100,100+200);
+//            GL11.glEnd();
+//            
+//            glfwSwapBuffers(window); // swap the color buffers           
+//            
+//            // Poll for window events. The key callback above will only be
+//            // invoked during this call.
+//            glfwPollEvents();
+//        }
     }
     
 }
