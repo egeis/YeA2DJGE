c31532eac68817b50576cfaad3c36eb88aaad33f
egeis <rcoan@callutheran.edu>
2015-04-22 14:13:15 -0700

    Moving Example to a new package for example code.

--- src/main/java/com/example/emitters/CodeEmitter.java
+++ src/main/java/com/example/emitters/CodeEmitter.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package main.java.com.YeAJG.fx.particle.emitters;
+package main.java.com.example.emitters;
 
 import java.util.HashMap;
 import java.util.List;
